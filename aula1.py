b9ed8649fa767fe2992fd1f9f45530f3b9790869
JuaoSousa <joaosilvx42@gmail.com>
2024-11-26 14:50:54 -0300

    primeiro commit

---
+++ aula1.py
@@ -0,0 +1,12 @@
+# = usado para escrever comentarios
+
+"""
+DocString = Não é considerado um comentario, o leitor lê ele mas não executa nada
+"""
+
+'''
+Usar para escrever suas notas
+'''
+
+print("Hello, World!")
+print("Hello, World!") 
