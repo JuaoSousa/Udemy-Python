027c3236e88a957e86ce7ced60437b95e9696300
JuaoSousa <joaosilvx42@gmail.com>
2024-12-05 10:34:44 -0300

    Add Try/Except
    Adicionei o Try e Except ao Codigo, para caso digite um número que não seja inteiro irá direto para o except

--- Exercicios/Saudacao.py
+++ Exercicios/Saudacao.py
@@ -3,20 +3,25 @@ Faça um programa que pergunte a hora ao usuário e, baseando-se no horário
 descrito, exiba a saudação apropriada. Ex. 
 Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 """
-hora = int(input("Digite a hora atual: "))
+hora = input("Digite a hora atual: ")
 
-print(f"voce digitou que são {hora}h")
+try:
+    hora_int = int(hora)
 
-if hora <= 11:
- print("Bom dia!!")
+    print(f"Você digitou que são {hora_int}h")
 
-elif hora <= 17:
- print("Boa tarde!!")
+    if hora_int <= 11:
+        print("Bom dia!!")  
 
-elif hora <= 23:
- print("Boa noite!!")
+    elif hora_int <= 17:
+        print("Boa tarde!!")
 
-else:
+    elif hora_int <= 23:
+        print("Boa noite!!")
+
+    else:
+        print("Voce digitou uma hora invalida!")
+
+except:
  print("Voce digitou uma hora invalida!")
 
-print("Fim do programa.")
