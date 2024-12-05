cefe068b69cb5b0ecc1a3c57b8c1c0b79b19eae6
JuaoSousa <joaosilvx42@gmail.com>
2024-12-05 13:22:44 -0300

    Create ExWhile

---
+++ Exercicios/While.py
@@ -0,0 +1,12 @@
+"""
+Executar uma ação enquanto uma condição for verdadeira 
+Loop Infinito -> Quando codigo não tem fim
+"""
+
+contador = 0
+
+while contador < 100:
+    contador += 1 #Isso é a mesma coisa que "contador = contador + 1"
+    print(f"Contador: {contador}")
+
+print("Fim do Programa")
