b59f321f8c85a6da74069e88a54ef21f33512431
JuaoSousa <joaosilvx42@gmail.com>
2024-12-13 13:53:02 -0300

    add exercicios

--- Exercicios/NumParesFor.py
+++ Exercicios/NumParesFor.py
@@ -1,8 +1,9 @@
-'''
-Faça um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50
-'''
+# '''
+# Faça um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50
+# '''
 
-for c in range(0,50+1,2):
-    print(c)
+# for c in range(0,50+1,2):
+#     print(c)
 
-print('Fim')
+# print('Fim')
+
---
+++ Exercicios/SomaDosPares.py
@@ -0,0 +1,6 @@
+t = int(input('Digite um número para ver tabuada: '))
+
+for c in range(1, 11, 1):
+    print(f'{t} x {c} = {t * c}')
+
+print("Fim da Tabuada")
